9cfcbfcebb98bcf1f38a517b4fede3a79bc268f5
altanemredemirci <altan_emre_1989@hotmail.com>
2026-06-21 15:24:54 +0300

    DAL\Concrete\EfCore\DataContext.cs yaz Proje referanslarını yap Migration-Update Database

---
+++ Makaan_DAL/Concrete/EfCore/DataContext.cs
@@ -0,0 +1,24 @@
+﻿using Makaan_Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Makaan_DAL.Concrete.EfCore
+{
+    internal class DataContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=202-3\\SQLEXPRESS;Database=MakaanDB;Trusted_Connection=true;TrustServerCertificate=true");
+        }
+
+        public DbSet<City> Cities { get; set; }
+        public DbSet<District> Districts { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductType> ProductTypes { get; set; }
+        public DbSet<ProductDetail> ProductDetail { get; set; }
+        public DbSet<Agency> Agencies { get; set; }
+        public DbSet<Slider> Sliders { get; set; }
+    }
+}
