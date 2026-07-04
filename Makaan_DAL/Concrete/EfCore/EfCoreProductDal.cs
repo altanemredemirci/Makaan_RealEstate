2cc443f250d87a3e31ea06f2980f3e4e6f159fe8
altanemredemirci <altan_emre_1989@hotmail.com>
2026-07-04 14:05:02 +0300

    EfCoreProductDal\GetAll revize _PartialProduct.cshtml

--- Makaan_DAL/Concrete/EfCore/EfCoreProductDal.cs
+++ Makaan_DAL/Concrete/EfCore/EfCoreProductDal.cs
@@ -3,12 +3,22 @@ using Makaan_Entity;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using System.Text;
 
 namespace Makaan_DAL.Concrete.EfCore
 {
     public class EfCoreProductDal : EfCoreGenericRepository<Product, DataContext>, IProductDal
     {
+
+        public override List<Product> GetAll(Expression<Func<Product, bool>> filter)
+        {
+           using(var context = new DataContext())
+            {
+                return context.Products.Where(i => i.Status).Include(i => i.ProductType).Include(i => i.City).ToList();
+            }
+        }
+
         public List<Product> GetPopularAll()
         {
             using (var context = new DataContext())
