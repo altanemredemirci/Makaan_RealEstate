e0cb59ecfbb9e082128739163317c1ac102d49ee
altanemredemirci <altan_emre_1989@hotmail.com>
2026-07-11 15:10:13 +0300

    AdminLTE hazır template al AdminController\Index() Index.cshtml LayoutAdmin.cshtml ViewComponent yapısına uyarla

---
+++ Makaan_UI/ViewComponents/LayoutAdmin/_AdminNavbarViewComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Makaan_UI.ViewComponents.LayoutAdmin
+{
+    public class _AdminNavbarViewComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ Makaan_UI/ViewComponents/LayoutAdmin/_AdminScriptViewComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Makaan_UI.ViewComponents.LayoutAdmin
+{
+    public class _AdminScriptViewComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
