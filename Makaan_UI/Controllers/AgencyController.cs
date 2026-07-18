e4bde7c9a9e8ad82d427678d57336588981feb8c
altanemredemirci <altan_emre_1989@hotmail.com>
2026-07-18 14:23:24 +0300

    AgencyController\Index() Index.cshtml

---
+++ Makaan_UI/Controllers/AgencyController.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using Makaan_BLL.Abstract;
+using Makaan_BLL.DTOs.AgencyDTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Makaan_UI.Controllers
+{
+    public class AgencyController : Controller
+    {
+        private readonly IAgencyService _agencyService;
+        private readonly IMapper _mapper;
+
+        public AgencyController(IAgencyService agencyService,IMapper mapper)
+        {
+            _agencyService = agencyService;
+            _mapper = mapper;
+        }
+
+        public IActionResult Index()
+        {
+            var agencies = _agencyService.GetAll();
+            var model = _mapper.Map<List<ResultAgencyDTO>>(agencies);
+            return View(model);
+        }
+    }
+}
